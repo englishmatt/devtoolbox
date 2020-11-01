e0355048ecd4fb3b7ad5692e2354b01934fc2611
Matt Titchener <matt.titchener@gmail.com>
2020-10-31 23:19:00 -0700

    Initial commit

---
+++ src/components/PageHeader.svelte
@@ -0,0 +1,18 @@
+<style>
+    header {
+        border-top: 5px solid var(--primary-accent-color);
+        box-sizing: border-box;
+        width: 100%;
+    }
+
+    .logo {
+        font-size: 1.8rem;
+        font-weight: 500;
+        margin: 1.4rem auto;
+        width: var(--main-content-width);
+    }
+</style>
+
+<header>
+    <div class="logo">HTTP Toolbox</div>
+</header>
