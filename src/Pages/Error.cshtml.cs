eee165325ea67fa9fcbe03cf3aa2b1600c284256
Matt Titchener <matt@titchener.net>
2020-11-16 20:57:57 -0800

    Change name from HTTP Toolbox to Dev Toolbox

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace HttpToolbox.Pages
+namespace DevToolbox.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
